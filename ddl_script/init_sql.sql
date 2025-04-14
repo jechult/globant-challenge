64de6065cf425bf8b9ef1f7a98957a83ab8da56c
jmlevano <xt6774@rimac.com.pe>
2025-04-14 10:42:10 -0500

    Update: Rename database name

--- ddl_script/init_sql.sql
+++ ddl_script/init_sql.sql
@@ -3,15 +3,15 @@ CREATE DATABASE IF NOT EXISTS companydb;
 USE companydb;
 
 CREATE TABLE IF NOT EXISTS department (
-    department_id INT 'Id of the department',
-    department_name VARCHAR(100) 'Name of the department',
+    department_id INT COMMENT 'Id of the department',
+    department_name VARCHAR(100) COMMENT 'Name of the department',
     creation_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT 'Row creation timestamp',
     PRIMARY KEY (department_id)
 );
 
 CREATE TABLE IF NOT EXISTS job (
-    job_id INT 'Id of the job',
-    job_name VARCHAR(100) 'Name of the job',
+    job_id INT COMMENT 'Id of the job',
+    job_name VARCHAR(100) COMMENT 'Name of the job',
     creation_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT 'Row creation timestamp',
     PRIMARY KEY (job_id)
 );
@@ -27,3 +27,5 @@ CREATE TABLE IF NOT EXISTS hired_employee (
     FOREIGN KEY (department_id) REFERENCES department(department_id),
     FOREIGN KEY (job_id) REFERENCES job(job_id)
 );
+
+GRANT ALL PRIVILEGES ON *.* TO 'jechu'@'%';
